f5b1df5f05f4c3d38e16e9bfcae2ef9b436d9da2
epopov <mr.demix@yandex.ru>
2023-12-05 19:12:21 +0300

    Added incomplete day03

--- day03/main.go
+++ day03/main.go
@@ -1,14 +1,131 @@
 package main
 
+import (
+	"log"
+	"strconv"
+	"unicode"
+)
+
 func main() {
 
 }
 
 // TODO: parse file to 2D array
-func parseFile(file string) [][]byte {
+func parseLine(array [][]int32, line string) [][]int32 {
+	newArrayLine := make([]int32, 0)
 
+	for _, char := range line {
+		newArrayLine = append(newArrayLine, char)
+	}
+
+	return append(array, newArrayLine)
+}
+
+type Cell struct {
+	x int
+	y int
+	v int32
 }
 
 // TODO: find all symbols in 2D array (store it like x/y/v)
+func findSymbols(array [][]int32) []Cell {
+	cells := make([]Cell, 0)
+
+	for i := 0; i < len(array); i++ {
+		for j := 0; j < len(array[i]); j++ {
+			if !unicode.IsDigit(array[i][j]) && string(array[i][j]) != "." {
+				//log.Printf("Symbol %c was found on %d:%d", array[i][j], i, j)
+				cells = append(cells, Cell{
+					x: i,
+					y: j,
+					v: array[i][j],
+				})
+			}
+		}
+	}
+
+	return cells
+}
+
 // TODO: find all numbers adjacent to symbols, collect them
+func numberAround(array [][]int32, i int, j int) []Cell {
+	numberCells := make([]Cell, 0)
+
+	// I don't found solution how to inline this boilerplate in loop. Golang doesn't have ternary operators :(
+	minX := 0
+	if i > 0 {
+		minX = -1
+	}
+	minY := 0
+	if j > 0 {
+		minY = -1
+	}
+	maxX := len(array)
+	if i < maxX {
+		maxX = 1
+	} else {
+		maxX = 0
+	}
+	maxY := len(array[0])
+	if j < maxY {
+		maxY = 1
+	} else {
+		maxY = 0
+	}
+
+	for dx := minX; dx <= maxX; dx++ {
+		for dy := minY; dy <= maxY; dy++ {
+			if dx != 0 || dy != 0 {
+				numberCells = append(numberCells, findNumber(array, i+dx, j+dy))
+			}
+		}
+	}
+
+	return numberCells
+}
+
+// TODO: helper function. Find whole number and its head
+func findNumber(array [][]int32, x int, y int) Cell {
+	//if !unicode.IsDigit(array[x][y]) {
+	//	log.Fatalf("initial value is not digit")
+	//	return Cell{}
+	//}
+
+	partials := make([]int32, 0)
+	// going to head of the number
+	xFirst := x
+	yFirst := 0
+	for i := y; i >= 0; i-- {
+		if unicode.IsDigit(array[x][i]) {
+			partials = append([]int32{array[x][i]}, partials...)
+		} else {
+			yFirst = i + 1
+			break
+		}
+	}
+
+	// going to back of the number
+	for i := y + 1; i < len(array[x]); i++ {
+		if unicode.IsDigit(array[x][i]) {
+			partials = append(partials, array[x][i])
+		} else {
+			//log.Printf("Char %c is not digit, break", array[x][y])
+			break
+		}
+	}
+
+	log.Printf("partials: %v", partials)
+	log.Printf("string partials: %v", string(partials))
+	atoi, err := strconv.Atoi(string(partials))
+	if err != nil {
+		return Cell{}
+	}
+
+	return Cell{
+		x: xFirst,
+		y: yFirst,
+		v: int32(atoi),
+	}
+}
+
 // TODO: Sum collected numbers
--- day03/main_test.go
+++ day03/main_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
+import (
+	"aoc2023/utils"
+	"log"
+	"testing"
+)
 
 func TestFirstPart(t *testing.T) {
+	array := make([][]int32, 0)
+	fileByLines := utils.ReadFileByLines("test_input.txt")
 
+	got := 0
+	expected := 4361
+	for _, line := range fileByLines {
+		array = parseLine(array, line)
+	}
+
+	//log.Printf("array: %c\n", array)
+	findSymbols(array)
+	//number := findNumber(array, 2, 3)
+	//log.Printf("Whole number = %d found with head on coords: %d:%d", number.v, number.x, number.y)
+
+	numbersAround := numberAround(array, 0, 3)
+	log.Printf("Found numbers around %d:%d - %v", 0, 3, numbersAround)
+
+	if got != expected {
+		t.Errorf("expected %d but got %d", expected, got)
+	}
 }
