fb4181d9debc3d931bf2b6b657371ffd3b35d879
epopov <mr.demix@yandex.ru>
2023-12-05 19:09:43 +0300

    Added incomplete day04

---
+++ day03/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+}
+
+// TODO: parse file to 2D array
+func parseFile(file string) [][]byte {
+
+}
+
+// TODO: find all symbols in 2D array (store it like x/y/v)
+// TODO: find all numbers adjacent to symbols, collect them
+// TODO: Sum collected numbers
---
+++ day03/main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestFirstPart(t *testing.T) {
+
+}
