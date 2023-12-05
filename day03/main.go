1d1652ad950869792e2d10b5d037bcaf043324e5
Evgeniy Popov <mr.demix@yandex.ru>
2023-12-05 20:14:39 +0300

    Day03 First part

--- day03/main.go
+++ day03/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"aoc2023/utils"
 	"log"
 	"strconv"
+	"time"
 	"unicode"
 )
 
 func main() {
+	start := time.Now()
+	log.Printf("First part result: %d\n", firstPart("day03\\input.txt"))
 
+	//log.Printf("Second part result: %d\n", secondPart())
+	elapsed := time.Since(start)
+	log.Printf("Execution time %s", elapsed)
 }
 
 // TODO: parse file to 2D array
@@ -76,7 +83,10 @@ func numberAround(array [][]int32, i int, j int) []Cell {
 	for dx := minX; dx <= maxX; dx++ {
 		for dy := minY; dy <= maxY; dy++ {
 			if dx != 0 || dy != 0 {
-				numberCells = append(numberCells, findNumber(array, i+dx, j+dy))
+				number := findNumber(array, i+dx, j+dy)
+				if number != nil {
+					numberCells = append(numberCells, *number)
+				}
 			}
 		}
 	}
@@ -85,11 +95,11 @@ func numberAround(array [][]int32, i int, j int) []Cell {
 }
 
 // TODO: helper function. Find whole number and its head
-func findNumber(array [][]int32, x int, y int) Cell {
-	//if !unicode.IsDigit(array[x][y]) {
-	//	log.Fatalf("initial value is not digit")
-	//	return Cell{}
-	//}
+func findNumber(array [][]int32, x int, y int) *Cell {
+	if !unicode.IsDigit(array[x][y]) {
+		//log.Printf("initial value is not digit")
+		return nil
+	}
 
 	partials := make([]int32, 0)
 	// going to head of the number
@@ -114,14 +124,14 @@ func findNumber(array [][]int32, x int, y int) Cell {
 		}
 	}
 
-	log.Printf("partials: %v", partials)
-	log.Printf("string partials: %v", string(partials))
+	//log.Printf("partials: %v", partials)
+	//log.Printf("string partials: %v", string(partials))
 	atoi, err := strconv.Atoi(string(partials))
 	if err != nil {
-		return Cell{}
+		return nil
 	}
 
-	return Cell{
+	return &Cell{
 		x: xFirst,
 		y: yFirst,
 		v: int32(atoi),
@@ -129,3 +139,53 @@ func findNumber(array [][]int32, x int, y int) Cell {
 }
 
 // TODO: Sum collected numbers
+func sumOfParts(numberCells []Cell) int32 {
+	var unique []Cell
+
+loop:
+	for _, v := range numberCells {
+		for i, u := range unique {
+			if v.x == u.x && v.y == u.y && v.v == u.v {
+				unique[i] = v
+				continue loop
+			}
+		}
+		unique = append(unique, v)
+	}
+
+	var result int32 = 0
+	for _, cell := range unique {
+		result += cell.v
+	}
+
+	return result
+}
+
+func firstPart(file string) int {
+	array := make([][]int32, 0)
+	fileByLines := utils.ReadFileByLines(file)
+
+	result := 0
+	for _, line := range fileByLines {
+		array = parseLine(array, line)
+	}
+
+	//log.Printf("array: %c\n", array)
+	symbols := findSymbols(array)
+	//log.Printf("Found symbols. %v", symbols)
+	//number := findNumber(array, 0, 4)
+	//log.Printf("Whole number = %d found with head on coords: %d:%d", number.v, number.x, number.y)
+
+	//numbersAround := numberAround(array, 1, 3)
+	//log.Printf("Found numbers around %d:%d - %+v", 0, 3, numbersAround)
+
+	cells := make([]Cell, 0)
+	for _, symbolCell := range symbols {
+		cells = append(cells, numberAround(array, symbolCell.x, symbolCell.y)...)
+	}
+
+	//log.Printf("Total found numbers around symbols: %v\n", cells)
+	result = int(sumOfParts(cells))
+
+	return result
+}
